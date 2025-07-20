5e33795331d7d35f86ec46e7851615e0becf8028
RealMarzz <suisi1972@gmail.com>
2025-07-20 13:02:42 +0100

    this is my C++ code from beginner to advanced from my C++ course

---
+++ Chapter-1-variables/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int age = 20;
+    // cout << age;
+    //  double age = 23983893039.5232323289;
+    // cout << age;
+    string firstName;
+    cout << "Enter your first name: ";
+    cin >> firstName;
+    cout << "Hello, " << firstName << endl;
+}
