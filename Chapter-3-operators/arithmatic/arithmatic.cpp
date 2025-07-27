e2f3eb89a747b069ceecfad7e48d6cc222844e8f
RealMarzz <suisi1972@gmail.com>
2025-07-27 14:01:19 +0100

    Chapter 3 operators

---
+++ Chapter-3-operators/arithmatic/arithmatic.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b;
+    a = 10;
+    b = 20;
+    // addition
+    cout << "Addition of a and b is " << a + b << endl;
+    // subtraction
+    cout << "Subtraction of a and b is " << a - b << endl;
+    // multiplication
+    cout << "Multiplication of a and b is " << a * b << endl;
+    // division
+    cout << "Division of a and b is " << a / b << endl;
+    // modulus
+    cout << "Modulus of a and b is " << a % b << endl;
+    int age = 20;
+    //increment operator
+    age++;
+    cout << "Age after increment is " << age << endl;
+    //decrement operator
+    age--;
+    age--;
+    cout << "Age after decrement is " << age << endl;
+    return 0;
+}
---
+++ Chapter-3-operators/assignment/assignment.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 50;
+    // age = age + 10;
+    // age += 10;
+
+    // age = age - 10;
+    age -= 10;
+
+     // age = age * 10;
+    age *= 10;
+
+     // age = age / 10;
+    age /= 10;
+
+     // age = age % 10;
+    age %= 10;
+    cout << age << endl;
+    return 0;
+}
